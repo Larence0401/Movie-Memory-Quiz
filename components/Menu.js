36eceea88fdf3052df37f9a3617a07ecef9e99e2
Lars Jensen <aventuras030@gmail.com>
2021-12-16 22:20:58 +0100

    c

--- components/Menu.js
+++ components/Menu.js
@@ -11,18 +11,18 @@ const Menu = ({handleSelect, startGame}) => {
 
     return (
         <div className="w-full max-w-xs h-full bg-black-500 flex flex-col px-4 py-8">
-            <label for="tiles" className="text-gray-300 italic ml-4 text-xl">Playing with ...</label>
+            <label htmlfor="tiles" className="text-gray-300 italic ml-4 text-xl">Playing with ...</label>
             <select name="tiles" id="tiles" className="p-1 mx-4 mb-8 mt-2 text-lg font-semibold leading-6" onChange={handleSelect}>
                 <option value="12">12 tiles</option>
                 <option value="18">18 tiles</option>
             </select>
-            <label for="tiles" className="text-gray-300 italic mx-4 text-xl">Category A:</label>
+            <label htmlfor="tiles" className="text-gray-300 italic mx-4 text-xl">Category A:</label>
             <select name="catA" id="catA" className="p-1 mx-4 mt-2 mb-8 text-lg font-semibold text-slate-900" onChange={handleSelect}>
                 <option value="poster_path">movie poster</option>
                 <option value="title">movie title</option>
                 <option value="release_date">year of release</option>
             </select>
-            <label for="tiles" className="text-gray-300 italic mx-4 text-xl">Category B:</label>
+            <label htmlfor="tiles" className="text-gray-300 italic mx-4 text-xl">Category B:</label>
             <select name="catB" id="catB" className="p-1 mx-4 mt-2  mb-8 text-lg font-semibold text-slate-900" onChange={handleSelect}>
                 <option value="poster_path">movie poster</option>
                 <option value="title">movie title</option>
